71b225dec7a3fd75e039149ae6e9ac607b446bda
Рева Дмитрий Владимирович <dmitriireva89@gmail.com>
2023-05-26 13:47:39 +0300

    add

--- src/gateways/gateways.js
+++ src/gateways/gateways.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchPost = async (id) => {
   const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/postss/${id}`
+    `https://jsonplaceholder.typicode.com/posts/${id}`
   );
   const post = response.data;
 
@@ -11,7 +11,7 @@ export const fetchPost = async (id) => {
 
 export const fetchPosts = async () => {
   const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/postss"
+    "https://jsonplaceholder.typicode.com/posts"
   );
 
   return response.data;
